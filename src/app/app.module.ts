24848ae73cb15e156bfbedcf941a4bdfca4e96bd
Antoine Humbert <antoine@skale-5.com>
2017-06-21 16:46:45 +0200

    update module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { GooglemapComponent } from './googlemap/googlemap.component';
+
+import { AgmCoreModule } from '@agm/core';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GooglemapComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyBV7ayzYxxlxvXsosIpgnQVtpLPwiP80Mw'
+    })
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, GooglemapComponent]
 })
 export class AppModule { }
