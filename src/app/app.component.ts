0c229e66b12a4f54fb45a0069771f03168837687
patak <pataky@hotmail.fr>
2017-06-22 17:04:00 +0200

    [feat] - handle authentication in appcomponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { HorizonService } from './horizon.service';
+import { AuthService } from './auth/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -12,7 +13,15 @@ export class AppComponent implements OnInit {
   table = this.horizon.table('menu');
   menus: Array<any> = [];
 
-  constructor(private horizon: HorizonService) { }
+  constructor(private horizon: HorizonService, public auth: AuthService) {
+    // Comment out this method call if using
+    // hash-based routing
+    auth.handleAuthentication();
+
+    // Uncomment this method call if using
+    // hash-based routing
+    // auth.handleAuthenticationWithHash();
+  }
 
   ngOnInit() {
     this.table
