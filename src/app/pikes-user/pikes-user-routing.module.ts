fa3347265fe6890c8a1e16ffb4031162ee5a37ab
patak <pataky@hotmail.fr>
2017-06-23 10:35:26 +0200

    [feat] - manage authentication guards for user account

--- src/app/pikes-user/pikes-user-routing.module.ts
+++ src/app/pikes-user/pikes-user-routing.module.ts
@@ -3,8 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { PikesUserComponent } from './pikes-user/pikes-user.component';
 
+import { AuthGuard } from './../auth/auth.guard';
+
 const routes: Routes = [
-  { path: 'user', component: PikesUserComponent }
+  {
+    path: 'user',
+    component: PikesUserComponent,
+    canActivate: [AuthGuard]
+  }
 ];
 
 @NgModule({
