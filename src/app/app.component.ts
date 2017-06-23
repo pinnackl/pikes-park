943d2f35296c4a06df6a7129e6531a8e8026ab56
athorel <alexis.thorel@gmail.com>
2017-06-23 15:33:35 +0200

    [feature] get user infos in base component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,22 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { HorizonService } from './horizon.service';
 import { AuthService } from './auth/auth.service';
+import { PikesUserService } from './pikes-user/pikes-user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [PikesUserService]
 })
 export class AppComponent implements OnInit {
   @ViewChild('sidenav') input: ElementRef;
   title = 'app';
   table = this.horizon.table('menu');
   menus: Array<any> = [];
+  profile: any;
 
-  constructor(private horizon: HorizonService, public auth: AuthService) {
+  constructor(private horizon: HorizonService, public auth: AuthService, public pikesUserService: PikesUserService) {
     // Comment out this method call if using
     // hash-based routing
     auth.handleAuthentication();
@@ -30,5 +33,9 @@ export class AppComponent implements OnInit {
       .subscribe(data => {
         this.menus = data;
       });
+    let nickname = localStorage.getItem('nickname');
+    this.pikesUserService.getUser('id', nickname).then(data => {
+      this.profile = data;
+    });
   }
 }
