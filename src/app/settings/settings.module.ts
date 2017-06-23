1c5c61694eec8b1bfa20836dc1a7febd8255e34a
Syu93 <herve.tutuaku@gmail.com>
2017-06-23 14:56:34 +0200

    [feature] try handle goe setting

--- src/app/settings/settings.module.ts
+++ src/app/settings/settings.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from '@angular/material';
 
 import { SettingsRoutingModule } from './settings-routing.module';
 import { SettingsComponent } from './settings/settings.component';
@@ -7,6 +9,8 @@ import { SettingsComponent } from './settings/settings.component';
 @NgModule({
   imports: [
     CommonModule,
+    MaterialModule,
+    BrowserAnimationsModule,
     SettingsRoutingModule
   ],
   declarations: [SettingsComponent]
--- src/app/settings/settings/settings.component.ts
+++ src/app/settings/settings/settings.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as permission from 'permissions.request';
 
 @Component({
   selector: 'app-settings',
@@ -10,6 +11,27 @@ export class SettingsComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  handlePermission(e) {
+    if (e.checked) {
+      permission.request({name:'geolocation'}).then(function(result) {
+        if (result.state == 'granted') {
+
+        } else if (result.state == 'prompt') {
+          navigator.geolocation.getCurrentPosition(e => {});
+        } else if (result.state == 'denied') {
+          // report(result.state);
+          // geoBtn.style.display = 'inline';
+        }
+        result.onchange = function() {
+          // report(result.state);
+        }
+      });
+    } else {
+      // permission.revoke({name:'geolocation'}).then(function(result) {
+      // });
+    }
   }
 
 }
